aafb10d27d037249c4b3ab87e64a1b1749b3d71b
keshav6606 <cinemahub77@gmail.com>
2025-07-02 06:58:14 -0700

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -6,19 +6,19 @@ from Backend import LOGGER
 
 load_dotenv(path.join(path.dirname(path.dirname(__file__)), "config.env"))
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "26954495"))
+    API_HASH = getenv("API_HASH", "2061c55207cfee4f106ff0dc331fe3d9")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7594581650:AAE5IIr5Yw8QhCUqe9B3dYgSzLqhDwzJMGU")
     PORT = int(getenv("PORT", "8000"))
     BASE_URL = getenv("BASE_URL", "0.0.0.0").rstrip('/')
     AUTH_CHANNEL = [channel.strip() for channel in (getenv("AUTH_CHANNEL") or "").split(",") if channel.strip()]
-    DATABASE = getenv("DATABASE", "").split(", ")
-    TMDB_API = getenv("TMDB_API", "")
-    IMDB_API = getenv("IMDB_API", "")
+    DATABASE = getenv("DATABASE", "mongodb+srv://cinemahub940:cinemahub940@cluster0.hmz84fc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").split(", ")
+    TMDB_API = getenv("TMDB_API", "6abcb6bb99fb77f33c37016a28866ed2")
+    IMDB_API = getenv("IMDB_API", "6abcb6bb99fb77f33c37016a28866ed2")
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
     UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-    MULTI_CLIENT = getenv("MULTI_CLIENT", "False").lower() == "true"
+    MULTI_CLIENT = getenv("MULTI_CLIENT", "True").lower() == "true"
     USE_CAPTION = getenv("USE_CAPTION", "False").lower() == "true"
     USE_TMDB = getenv("USE_TMDB", "False").lower() == "true"
-    OWNER_ID = int(getenv("OWNER_ID", "5422223708"))
+    OWNER_ID = int(getenv("OWNER_ID", "7045947967"))
     USE_DEFAULT_ID = getenv("USE_DEFAULT_ID", None)
