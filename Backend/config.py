847a9f1e5b33042c34d086d9e53c3ba7bd996b03
keshav6606 <cinemahub77@gmail.com>
2025-07-22 09:51:02 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -10,7 +10,7 @@ class Telegram:
     API_HASH = getenv("API_HASH", "2061c55207cfee4f106ff0dc331fe3d9")
     BOT_TOKEN = getenv("BOT_TOKEN", "7594581650:AAE5IIr5Yw8QhCUqe9B3dYgSzLqhDwzJMGU")
     PORT = int(getenv("PORT", "8000"))
-    BASE_URL = getenv("BASE_URL", "0.0.0.0").rstrip('/')
+    BASE_URL = getenv("BASE_URL", "https://marked-boa-filetolink7-180f3379.koyeb.app").rstrip('/')
     AUTH_CHANNEL = [channel.strip() for channel in (getenv("AUTH_CHANNEL") or "").split(",") if channel.strip()]
     DATABASE = getenv("DATABASE", "mongodb+srv://keshavrajlkr2:JKAY9Xji3uwsY78D@cluster0.fqosisu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").split(", ")
     TMDB_API = getenv("TMDB_API", "")
@@ -21,4 +21,4 @@ class Telegram:
     USE_CAPTION = getenv("USE_CAPTION", "False").lower() == "true"
     USE_TMDB = getenv("USE_TMDB", "False").lower() == "true"
     OWNER_ID = int(getenv("OWNER_ID", "7045947967"))
-    USE_DEFAULT_ID = getenv("6285713858", None)
+    USE_DEFAULT_ID = getenv("USE_DEFAULT_ID", None)
